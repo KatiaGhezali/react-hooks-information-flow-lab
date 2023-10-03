ae0219b1cd43efbfcb2db97df6d5b6c9502f2424
Katia Ghezali <ghezalikatia@hotmail.com>
2023-10-03 14:00:15 +0100

    Completed Assignment

---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+
+function Filter({ handleCategoryChange}) {
+
+
+
+    return (
+
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+    );
+}
+
+
+export default Filter;
